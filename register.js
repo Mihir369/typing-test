72f07d404720120b3ea1bbebf352bbcb91a743ed
Mihir369 <136890654+Mihir369@users.noreply.github.com>
2026-04-08 07:50:28 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAfevwEbOJHtdyyxV1T1HPbL_nfaSOw6s",
+  authDomain: "swiftkeys-cedb8.firebaseapp.com",
+  projectId: "swiftkeys-cedb8",
+  storageBucket: "swiftkeys-cedb8.firebasestorage.app",
+  messagingSenderId: "490135968817",
+  appId: "1:490135968817:web:36b1642bb42620e2a80910"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
